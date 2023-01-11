40c3129c408b6fffeb7311f8bb7a51a671b14a5f
marcoscustodiodias <marcosdias.pvai@gmail.com>
2023-01-11 16:00:56 -0300

    Primeiro App em Python

---
+++ .py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.uix.button import Button
+
+kivy.require('1.9.1')
+
+var = 0
+def soma_um(instance):
+    global var
+    var += 1
+    instance.text = str(var)    
+    
+class MeuApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical',
+                padding=[40, 20, 40, 20])
+        
+        layout.add_widget(Label(text='Olá do Kivy!'))
+        btn = Button(text='Pressione-me!', size=(100,50))
+        
+        btn.bind(on_press=soma_um)
+        layout.add_widget(btn)
+        return layout 
+    
+if __name__ == '__main__':
+    MeuApp().run()
